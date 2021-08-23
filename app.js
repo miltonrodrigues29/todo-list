810630440cf3d423c981be2c93e4cf54672818db
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-08-24 00:05:08 +0530

    To-Do List Version1

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const ejs = require("ejs");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+var items = [];
+var workItems =[];
+app.get("/",function(req,res)
+{
+ var day="";
+ const today = new Date();
+ var current_day =  today.getDay();
+
+ var options ={
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+ }
+
+ day = today.toLocaleDateString("en-US",options);
+ console.log(day)
+ res.render("list",{ listTitle: day, items: items})
+})
+
+app.post("/",function(req,res)
+{
+
+  var new_item = req.body.newitem;
+  console.log(req.body.list);
+  if(req.body.list == "Work")
+  {
+    workItems.push(new_item);
+    res.redirect("/work");
+  }
+  else
+  {
+    console.log("Iam in else part");
+    items.push(new_item);
+    res.redirect("/");
+  }
+
+
+} );
+
+app.get("/work",function(req,res)
+{
+  res.render("list",{listTitle :"Work List", items:workItems })
+})
+
+app.listen(3000, function(req,res)
+{
+  console.log("Server running at port 3000")
+})
